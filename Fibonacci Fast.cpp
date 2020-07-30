4d3dc375c4a3c817373cba8055367a405d938f45
Shyam Kumar  S V <34570424+shyamsvs@users.noreply.github.com>
2020-07-30 08:43:24 +0530

    Create Fibonacci Fast.cpp

---
+++ Fibonacci Fast.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <assert.h>
+#include <bitset>
+#include <chrono>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <istream>
+#include <map>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <cmath>
+#include <array>
+#include <complex>
+
+using namespace std;
+
+pair<int, int> fib (int n) {
+    if (n == 0)
+        return {0, 1};
+    auto p = fib(n >> 1);
+    int c = p.first * (2 * p.second - p.first);
+    int d = p.first * p.first + p.second * p.second;
+    if (n & 1)
+        return {d, c + d};
+    else
+        return {c, d};
+}
+
+int main(){
+    pair<int, int> ans = fib(14);
+    cout<<ans.first<<" "<<ans.second<<endl;
+    return 0;
+}
